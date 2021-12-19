1455cbbdca19c9287bc4a55cc6ea0aa8c14e2c1d
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-19 13:24:44 -0500

    login validations are a success

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,11 +4,27 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+//Validations for Login
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 // Log in
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -57,5 +73,20 @@ router.delete(
 //     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
 //   }
 // }).then(res => res.json()).then(data => console.log(data));
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
 
 module.exports = router;
