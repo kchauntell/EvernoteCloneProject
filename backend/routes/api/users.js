dbb76aedfb938bc41a2ed478c247ddd4c27e95bc
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-19 13:32:28 -0500

    sign up validations are working

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,11 +4,74 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage(`Please Provide a First Name`)
+    .isLength({ max: 50 })
+    .withMessage("First Name cannot be longer than 50 characters."),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage(`Please Provide a Last Name`)
+    .isLength({ max: 50 })
+    .withMessage("First Name cannot be longer than 50 characters."),
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.')
+    .custom((value) => {
+      return User.findOne({
+        where: {
+          email: value,
+        },
+      }).then((user) => {
+        if (user) {
+          return Promise.reject("Provided email address already in use.");
+        }
+      });
+    }),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.')
+    .isLength({ max: 30 })
+    .withMessage("Username cannot be longer than 30 characters.")
+    .custom((value) => {
+      return User.findOne({
+        where: {
+          username: value,
+        },
+      }).then((user) => {
+        if (user) {
+          return Promise.reject("Provided username already in use.");
+        }
+      });
+    }),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+  handleValidationErrors
+];
+
 
 // Sign up
 router.post(
   '/',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { firstName, lastName, email, password, username } = req.body;
     const user = await User.signup({ firstName, lastName, email, username, password });
